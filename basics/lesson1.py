e661d214a2432d37082ce4a1a6d93bc6a2218df9
Kira Telegina <kira.telegina@gmail.com>
2026-04-17 21:16:04 +0200

    add typing annotations for functions in lesson 1

--- basics/lesson1.py
+++ basics/lesson1.py
@@ -1,3 +1,5 @@
+from typing import List
+
 name = "Ann"
 age = 24
 city = "Moscow"
@@ -5,3 +7,14 @@ city = "Moscow"
 print(f"My name is {name}.")
 print(f"I'm {age} years old.")
 print(f"I live in {city}.")
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def get_full_name(first_name: str, last_name: str) -> str:
+    return first_name + " " + last_name
+
+def print_items(items: List[str]) -> None:
+    for item in items:
+        print(item)
