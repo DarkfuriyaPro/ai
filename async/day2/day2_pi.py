3c437d8b49fbb12c1224606e5bda25fff95262c4
Kira Telegina <kira.telegina@gmail.com>
2026-04-21 21:41:15 +0200

    Add error handling and status checks for async API requests

--- async/day2/day2_pi.py
+++ async/day2/day2_pi.py
@@ -1,26 +1,27 @@
 import aiohttp
 import asyncio
 
-async def fetch_post1(session):
-    async with session.get("https://jsonplaceholder.typicode.com/posts/1") as response:
-        data = await response.json()
-        return data
+urls = ["https://jsonplaceholder.typicode.com/posts/1", "https://jsonplaceholder.typicode.com/posts/2", "https://jsonplaceholder.typicode.com/posts/3"]
 
-
-async def fetch_post2(session):
-    async with session.get("https://jsonplaceholder.typicode.com/posts/2") as response:
-        data = await response.json()
-        return data
-
-async def fetch_post3(session):
-    async with session.get("https://jsonplaceholder.typicode.com/posts/3") as response:
-        data = await response.json()
-        return data
+async def fetch(url, session):
+        try:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data
+                else:
+                    return None
+        except:
+            print("Request failed")
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        await asyncio.gather(fetch_post1(session), fetch_post2(session), fetch_post3(session))
+        tasks = []
+        for url in urls:
+            tasks.append(fetch(url, session))
+        results =  await asyncio.gather(*tasks)
 
-
+        for r in results:
+            print(r)
 
 asyncio.run(main())
